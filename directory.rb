8fe7ec4047b3a0a96d30bd9d384c377c60f7df9f
NineInchNade <nineinchnade@gmail.com>
2014-08-05 18:42:33 +0100

    #5

--- directory.rb
+++ directory.rb
@@ -4,30 +4,30 @@
 student_count = 22 
 #first, we print the list of students 
 students = [
-"Javier Silverio",
-"Elliot Lewis",
-"Ben Tillett",
-"Vincent Koch",
-"Michelle Ballard",
-"Nick Roberts",
-"Tatiana Soukiassian", 
-"Mervé Silk",
-"Albert Vallverdu",
-"Lovis Schultze",
-"Henry Stanley",
-"Spike Lindsey",
-"Ruth Earle",
-"Andy Gates",
-"Faisal Aydarus",
-"Ethel Ng",
-"Kevin Daniells",
-"Maya Driver",
-"Leopold Kwok",
-"James McNeil",
-"Jerome Pratt",
-"David Wickes",
-"Chris Oatley",
-"Marc Singh"
+{ :name =>"Javier Silverio", :cohort => :august},
+{ :name =>"Elliot Lewis", :cohort =>:august},
+{ :name =>"Ben Tillett", :cohort =>:august},
+{ :name =>"Vincent Koch", :cohort =>:august},
+{ :name =>"Michelle Ballard", :cohort =>:august},
+{ :name =>"Nick Roberts", :cohort =>:august},
+{ :name =>"Tatiana Soukiassian", :cohort =>:august},
+{ :name =>"Mervé Silk", :cohort => :august},
+{ :name =>"Albert Vallverdu", :cohort => :august},
+{ :name =>"Lovis Schultze", :cohort => :august},
+{ :name =>"Henry Stanley", :cohort => :august},
+{ :name =>"Spike Lindsey", :cohort => :august},
+{ :name =>"Ruth Earle",:cohort => :august},
+{ :name =>"Andy Gates", :cohort => :august},
+{ :name =>"Faisal Aydarus", :cohort => :august},
+{ :name =>"Ethel Ng", :cohort => :august},
+{ :name =>"Kevin Daniells",:cohort =>  :august},
+{ :name =>"Maya Driver", :cohort => :august},
+{ :name =>"Leopold Kwok", :cohort => :august},
+{ :name =>"James McNeil", :cohort => :august},
+{ :name => "Jerome Pratt", :cohort => :august},
+{ :name =>"David Wickes",:cohort =>  :august},
+{ :name =>"Chris Oatley", :cohort => :august},
+{ :name =>"Marc Singh",:cohort =>  :august},
 ]
 
 def print_header 
@@ -35,8 +35,9 @@ def print_header
 	puts "-------------------------------------------"
 end
 
-def out(names)
-    names.each { |x| puts x} 
+def print(students)
+    students.each { |student| 
+    	puts "#{student[:name]} (#{student[:cohort]} cohort)" }
     end 
 
 def print_footer(names)
@@ -44,5 +45,5 @@ def print_footer(names)
 end
 
 print_header
-out(students) 
+print(students) 
 print_footer(students) 
